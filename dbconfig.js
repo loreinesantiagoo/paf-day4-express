5e8b5afb7aff252d9f251fc39f257984ef17aea5
loreine <loreinesantiagoo@gmail.com>
2018-10-12 00:20:47 +0800

    first commit, done

---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    host:'localhost',
+    port: 3306,
+    user: 'paf2018', 
+    password: 'fredPaf2018',
+    database: 'northwind',
+    connectionLimit: 4
+}
+
+module.exports = CONFIG;
